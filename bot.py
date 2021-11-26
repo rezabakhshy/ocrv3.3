ca0684795e7ef8f86023ef8e43c036b5e0e0fc20
rezabakhshy <87357982+rezabakhshy@users.noreply.github.com>
2021-11-26 23:21:10 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from googletrans import Translate
 from typing import Text
 from pyrogram import Client,filters
 import os
@@ -12,12 +13,21 @@ def ocr(client, message):
     last_id = message.chat.id
     text=message.text
     text2=text.split()[0]
+    text=text.replace(text2,"")
     if text2=="!ocr":
         text=message.reply_to_message.photo.file_id
         client.download_media(text,"test.jpg")
         client.send_photo("ocr_prov_bot","downloads/test.jpg")
         os.remove("downloads/test.jpg")
-       
+    elif text2=="!tran":
+        src=text.split()[0]
+        text=text.replace(src,"")
+        dest=text.split()[0]
+        text=text.replace(dest,"")
+        translator = Translate()
+        result = translator.translate(text, src=src, dest=dest)
+        client.send_message(chat_id=message.chat.id,text=result,reply_to_message_id=message_id)
+
 @app.on_message(filters.chat(2143804610))
 def forward(client,message):
     text=message.text
